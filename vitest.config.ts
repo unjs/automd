44c880b406c107e04586de029bf59c6d3cc3dd45
Pooya Parsa <pooya@pi0.io>
2024-02-22 00:40:16 +0100

    update vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    reporters: ["hanging-process"],
+    // reporters: ["hanging-process"],
     // globalSetup: "./test/_setup.ts",
   },
 });
