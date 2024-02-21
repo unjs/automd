41a621da4168d08c968951c55bcab99dd389cb17
Pooya Parsa <pooya@pi0.io>
2024-02-22 00:35:41 +0100

    chore: disable global setup for now

--- test/_setup.ts
+++ test/_setup.ts
@@ -1,12 +1,12 @@
-// import { automd } from "../src";
+import { automd } from "../src";
 
-// console.log("Starting automd with watcher on repo...");
-// const { unwatch } = await automd({
-//   watch: true,
-// });
+console.log("Starting automd with watcher on repo...");
+const { unwatch } = await automd({
+  watch: true,
+});
 
 export const seup = async () => {};
 
 export const teardown = async () => {
-  // await unwatch?.();
+  await unwatch?.();
 };
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    globalSetup: "./test/_setup.ts",
+    // globalSetup: "./test/_setup.ts",
   },
 });
