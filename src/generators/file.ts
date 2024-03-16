48b567c15e2c96ada3e678faaeec23d5f19598fa
Horu <73709188+HigherOrderLogic@users.noreply.github.com>
2024-03-16 23:03:52 +0700

    feat(file): new implementation
    Autofix, fix now!

--- src/generators/file.ts
+++ src/generators/file.ts
@@ -10,22 +10,28 @@ export const file = defineGenerator({
     const fullPath = resolvePath(args.src, { url, dir: config.dir });
     let contents = await readFile(fullPath, "utf8");
 
-    if (args.fromLine) {
-      if (args.fromLine < 0) {
-        throw new Error("line index can not be smaller than 1");
-      }
-      if (!args.toLine) {
-        throw new Error("fromLine exists but toLine is not defined");
+    if (args.lines) {
+      if (!/^(\d+)?:(\d+)?$/.test(args.lines)) {
+        throw new Error("invalid lines format");
       }
 
+      const [_startLine, _endLine] = args.lines.split(":");
+
       const lines = contents.split("\n");
-      if (args.toLine > lines.length) {
+
+      const startLine = _startLine === "" ? 0 : Number(_startLine);
+      const endLine = _endLine === "" ? lines.length : Number(_endLine);
+
+      if (startLine < 1) {
+        throw new Error("first line's index can not be smaller than 1");
+      }
+      if (endLine > lines.length) {
         throw new Error(
-          `line ${args.toLine} exceeds the total line number of the file`,
+          `line ${endLine} was specified while file ${fullPath} has only ${lines.length} lines`,
         );
       }
 
-      contents = lines.slice(args.fromLine - 1, args.toLine - 1).join("\n");
+      contents = lines.slice(startLine - 1, endLine - 1).join("\n");
     }
 
     if (args.code) {
